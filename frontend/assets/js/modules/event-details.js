6f1f9d680acb1c48ea85c35edd583024cb284170
João Gabriel <jgabriel.leite@outlook.com>
2024-12-24 17:46:50 -0300

    fix: redirect to checkout

--- frontend/assets/js/modules/event-details.js
+++ frontend/assets/js/modules/event-details.js
@@ -1,7 +1,7 @@
-// Alerta de ticket adicionado ao carrinho
 const link = document.querySelector('.btn.event');
     
 link.addEventListener("click", (event) => {
+    event.preventDefault();
     alert("Ingresso adicionado ao carrinho, conclua sua compra!");
     window.location.href = "../checkout/";
 });
