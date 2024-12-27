c4c3d4c04c4ace6002910b04816229579d639914
João Gabriel <jgabriel.leite@outlook.com>
2024-12-26 21:48:35 -0300

    fix: buy event btn's

--- frontend/assets/js/modules/event-details.js
+++ frontend/assets/js/modules/event-details.js
@@ -1,7 +1,9 @@
-const link = document.querySelector('.btn.event');
-    
-link.addEventListener("click", (event) => {
-    event.preventDefault();
-    alert("Ingresso adicionado ao carrinho, conclua sua compra!");
-    window.location.href = "../checkout/";
+const links = document.querySelectorAll('.btn.event');
+
+links.forEach(link => {
+    link.addEventListener("click", (event) => {
+        event.preventDefault();
+        alert("Ingresso adicionado ao carrinho, conclua sua compra!");
+        window.location.href = "../checkout/";
+    });
 });
